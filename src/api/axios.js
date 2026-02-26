21898db20bce343dc7b528ba5ce7008d95f67f12
md-sonu07 <mdsonu824153@gmail.com>
2026-02-26 20:09:25 +0530

    fix: include Bearer token in axios requests

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,4 +7,18 @@ const api = axios.create({
     withCredentials: true,
 });
 
+// Add request interceptor to include token
+api.interceptors.request.use(
+    (config) => {
+        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+        if (userInfo && userInfo.token) {
+            config.headers.Authorization = `Bearer ${userInfo.token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default api;
